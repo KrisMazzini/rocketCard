c809df2c87e89c278d6e80ec449a4f543c91ed6b
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-21 20:03:54 -0300

    add card container

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export function Card() {
+    return (
+        <div className="card-container">
+            <div className="card">
+                
+            </div>
+        </div>
+    )
+}
