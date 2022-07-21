f10457f855ab70544ef42ffc40f02e419c997a81
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-21 19:36:34 -0300

    fetch profile data from github api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './styles.css'
 
 export function Home() {
+
+    const [profile, setProfile] = useState()
+
+    useEffect(() => {
+        async function fetchGithubData() {
+            const githubAPI = "https://api.github.com/users/KrisMazzini"
+            const response = await axios.get(githubAPI)
+
+            setProfile(response.data)
+        }
+
+        fetchGithubData().catch(err => console.error("API not found"))
+    }, [])
+
     return (
-        <></>
+        <>
+        </>
     )
 }
