9403070f0fd1b89ac1d47f4026114d7dd07ee8d9
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-21 21:28:53 -0300

    add card avatar

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -10,6 +10,7 @@ export function Card({profile}) {
                     </div>
                     <h1>{profile.login}</h1>
                 </header>
+                <img src={profile.avatar_url} alt="avatar" className="avatar" />
                 <footer>
                     <img src="/logo.svg" alt="logo" className='logo' />
                     <h2>Rocketcard</h2>
